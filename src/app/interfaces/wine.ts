d59c2fe62997119430fafa5f10a54eccf17d3a40
Bence Cserfalvi <bence.cserfalvi@gmail.com>
2019-07-18 15:00:22 +0200

    Add wine-card-errors & wine-dto if & wine if & special-offer service

---
+++ src/app/interfaces/wine.ts
@@ -0,0 +1,12 @@
+export interface WineCard {
+
+  // image: Image;
+  id: number,
+  name: string,
+  price: number,
+  salePrice: number,
+  rating: number, //null ha nincs
+  numberOfRating: number,
+
+
+}
