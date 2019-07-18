49ae42318bbf044e68b95d2f2dccd2bf62dfec06
Gabor Gerencser <kispeka94@gmail.com>
2019-07-18 14:35:58 +0200

    added dropdown hover upon product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { SpecialOffersComponent } from './components/landing-page/special-offers/special-offers.component';
-import {RedWhiteCategoriesComponent} from './components/landing-page/red-white-categories/red-white-categories.component';
+import { RedWhiteCategoriesComponent } from './components/landing-page/red-white-categories/red-white-categories.component';
+import { ContactComponent } from './components/footer/contact/contact.component';
 
 const routes: Routes = [
-    { path: '', pathMatch: 'full', component: LandingPageComponent }
+  { path: '', pathMatch: 'full', component: LandingPageComponent },
   { path: 'landing-special-offers', component: SpecialOffersComponent },
-  { path: '', component: LandingPageComponent },
   { path: 'category', component: RedWhiteCategoriesComponent },
+
+  { path: 'contact', component: ContactComponent },
 
 
 ]
