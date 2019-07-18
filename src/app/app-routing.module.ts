4da1070c8f9251cbab7e942d4ea7f59483733a7b
Jeno Reich <reichjeno@gmail.com>
2019-07-18 14:57:43 +0100

    added category url-s

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { SpecialOffersComponent } from './components/landing-page/special-offers/special-offers.component';
 import {RedWhiteCategoriesComponent} from './components/landing-page/red-white-categories/red-white-categories.component';
+import { ProductsComponent } from './components/products/products.component';
+
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'landing-special-offers', component: SpecialOffersComponent },
   { path: '', component: LandingPageComponent },
   { path: 'category', component: RedWhiteCategoriesComponent },
-
+  {path: 'products/category-white', component: ProductsComponent },
+  {path: 'products/category-red', component: ProductsComponent }
 
 ]
 
