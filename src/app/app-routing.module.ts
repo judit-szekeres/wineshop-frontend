9e72e41527501e7e2d5dbaf383fcdb4443528130
Gabor Gerencser <kispeka94@gmail.com>
2019-07-18 15:12:02 +0200

    added afterview scroll top

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { LandingPageComponent } from './components/landing-page/landing-page.com
 import { SpecialOffersComponent } from './components/landing-page/special-offers/special-offers.component';
 import { RedWhiteCategoriesComponent } from './components/landing-page/red-white-categories/red-white-categories.component';
 import { ContactComponent } from './components/footer/contact/contact.component';
+import { PolicyComponent } from './components/footer/policy/policy.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: LandingPageComponent },
   { path: 'landing-special-offers', component: SpecialOffersComponent },
   { path: 'category', component: RedWhiteCategoriesComponent },
-
+  { path: 'policy', component: PolicyComponent },
   { path: 'contact', component: ContactComponent },
 
 
