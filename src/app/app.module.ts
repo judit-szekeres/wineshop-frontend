233e326f931bbccbba7467866ee539fcc8f58da5
gyurani <istvan.gyuran@gmail.com>
2019-07-19 10:22:56 +0200

    login-registration modal html added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { SpecialOffersComponent } from './components/landing-page/special-offers
 import { LoginRegistrationModalComponent } from './components/login-registration-modal/login-registration-modal.component';
 import { PolicyComponent } from './components/footer/policy/policy.component';
 import { ContactComponent } from './components/footer/contact/contact.component';
+import { LoginModalComponent } from './components/login-registration-modal/login-modal/login-modal.component';
+import { RegistrationModalComponent } from './components/login-registration-modal/registration-modal/registration-modal.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { ContactComponent } from './components/footer/contact/contact.component'
     SpecialOffersComponent,
     LoginRegistrationModalComponent,
     PolicyComponent,
-    ContactComponent
+    ContactComponent,
+    LoginModalComponent,
+    RegistrationModalComponent
   ],
   imports: [
     BrowserModule,
