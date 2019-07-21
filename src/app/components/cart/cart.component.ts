9fd25b180695d7b18b06c545e5461b1329ed51fa
Jeno Reich <reichjeno@gmail.com>
2019-07-21 21:33:43 +0100

    modified cart component: cart-table component represents 1 row in the table

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductCardComponent } from '../product-card/product-card.component';
+import {CartService} from '../../services/cart.service';
+import { WineCard } from "../../interfaces/wine";
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.css"]
 })
 export class CartComponent implements OnInit {
+  
+  choosenProducts:WineCard[];
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(public service:CartService) {
+    this.choosenProducts=this.service.addedProduct;
   }
 
+
+  ngOnInit() {
+
+  }
 }
