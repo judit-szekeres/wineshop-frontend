b79145fc9e7f3c5f8cc2b437d2d1ee0be5e8dcf4
gyurani <istvan.gyuran@gmail.com>
2019-07-22 20:05:54 +0200

    Login updated

--- src/app/services/user-http.service.ts
+++ src/app/services/user-http.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../interfaces/user';
+import { Usercredentials } from '../interfaces/usercredentials'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserHttpService {
 
-  private readonly URL = 'http://192.168.1.231:8080/registration';
+  private readonly URL = 'http://192.168.1.231:8080';
 
   constructor(private http: HttpClient) { }
 
   addUser(user: User): Promise<User> {
-    return this.http.post(this.URL, user , { withCredentials: true })
+    return this.http.post(this.URL + '/registration', user , { withCredentials: true })
       .toPromise() as Promise<User>;
+  }
+
+  loginUser(user: Usercredentials): Promise<null> {
+    return this.http.post(this.URL + '/login', user , { withCredentials: true })
+      .toPromise() as Promise<null>;
   }
 }
