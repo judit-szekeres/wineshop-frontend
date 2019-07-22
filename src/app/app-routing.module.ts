cd064285a975ca4994af0766d40625249e800309
rado <rraddo7@gmail.com>
2019-07-22 10:27:50 +0200

    bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,7 @@ import { ContactComponent } from './components/footer/contact/contact.component'
 import { PolicyComponent } from './components/footer/policy/policy.component';
 import { TermOfServicesComponent } from './components/footer/term-of-services/term-of-services.component';
 import { LoginRegistrationModalComponent } from './components/login-registration-modal/login-registration-modal.component';
-<<<<<<< HEAD
-import { CartComponent } from './components/cart/cart.component';
-=======
 import {CartComponent} from './components/cart/cart.component';
->>>>>>> master
-
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: LandingPageComponent },
@@ -26,12 +21,8 @@ const routes: Routes = [
   { path: 'term-of-services', component: TermOfServicesComponent },
   { path: 'log-reg', component: LoginRegistrationModalComponent },
   { path: 'products', component: ProductsComponent },
-<<<<<<< HEAD
   { path: 'cart', component: CartComponent },
-=======
-  { path: 'cart', component: CartComponent}
 
->>>>>>> master
 ]
 
 
