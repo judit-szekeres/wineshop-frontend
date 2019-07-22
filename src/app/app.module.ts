c08aa39f2c84695263501a4955c24f5dc045ad11
Bence Cserfalvi <bence.cserfalvi@gmail.com>
2019-07-22 09:30:05 +0200

    Bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -52,7 +52,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CarouselModule
+    CarouselModule,
     HttpClientModule,
     FormsModule
   ],
