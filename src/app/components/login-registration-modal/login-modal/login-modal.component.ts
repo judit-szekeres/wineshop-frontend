ce8e872f369cff0df5cd338f59d69fc73e66e77e
Gabor Gerencser <kispeka94@gmail.com>
2019-07-23 13:00:56 +0200

    login html part done

--- src/app/components/login-registration-modal/login-modal/login-modal.component.ts
+++ src/app/components/login-registration-modal/login-modal/login-modal.component.ts
@@ -13,6 +13,12 @@ export class LoginModalComponent implements OnInit {
   user: Usercredentials;
   errors: string;
   badData: boolean;
+  emailNotValidEmail : boolean;
+  emailNotRegistered : boolean;
+  invalidPassword : boolean;
+  emailNotValidEmailMessage = 'Invalid email address!';
+  emailNotRegisteredMessage = 'Not registered email address!'
+  invalidPasswordMessage = 'Incorrect password!';
 
   constructor(private userService: UserHttpService, private router: Router) {
     this.user = {
@@ -20,22 +26,35 @@ export class LoginModalComponent implements OnInit {
       password: ''
     }
     this.errors = '';
-    this.badData = false;
+    this.emailNotRegistered = false;
+    this.emailNotValidEmail = false;
+    this.invalidPassword = false;
   }
 
   ngOnInit() {
   }
   submit(): void {
-    this.userService.loginUser(this.user).then(() => {
-      this.router.navigate(['']);
-    }).catch(userError => {
-        console.log(userError);
-        this.errors = userError.status;
-        if(this.errors == '401'){
-            this.badData = true;
-            console.log('Hibás login.')
-        }
-      });
+      this.emailNotValidEmail = false;
+      this.invalidPassword = false;
+    if(! this.isEmailValid() ){
+        this.emailNotValidEmail = true;
+    }else{
+        this.userService.loginUser(this.user).then(() => {
+          this.router.navigate(['']);
+        }).catch(userError => {
+            console.log(userError);
+            this.errors = userError.status;
+            if(this.errors == '401'){
+                this.isEmailValid();
+                console.log('Hibás login.')
+            }
+          });
+    }
+
   }
 
+  isEmailValid(): boolean {
+        const emailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return this.user.email !== '' && emailRegexp.test(this.user.email);
+    }
 }
