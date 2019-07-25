bcb6f4bece86bbe30dcc4b87aa3501216f0930cb
rado <rraddo7@gmail.com>
2019-07-26 01:32:17 +0200

    added contact features

--- src/app/components/footer/contact/contact.component.ts
+++ src/app/components/footer/contact/contact.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Contact } from 'src/app/interfaces/contact';
 import { HttpClient } from '@angular/common/http';
 import { ContactService } from 'src/app/services/contact.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -13,29 +14,42 @@ export class ContactComponent implements OnInit {
 
   contact: Contact;
 
-  constructor(private contactService: ContactService) {
+  notValidName: boolean;
+  notValidNameMessage = 'Not valid name, please try again.';
+
+  constructor(private contactService: ContactService, private router: Router) {
     this.contact = {
       name: '',
       email: '',
       phone: '',
       message: ''
     };
+    this.notValidName = false;
   }
-
-    /*this.http.post(this.URL, this.contact).toPromise().then(this.mail: any => {
-      this.json = JSON.stringify(mail.json); mail az ami a szerverről jön?*});*/
-
 
   ngOnInit() {
   }
 
-  submit() {
-    this.contactService.send(this.contact).then(() => {
-      // köszönjük!
-      // contact adatok kirítése
-    }).catch(() => {
-      // próbálja később
-    });
-  }
+  submit(): void {
+    this.notValidName = false;
+    if(! this.isValidName() ){
+      this.notValidName = true;
+    }else{
+      this.contactService.send(this.contact).then( response => {
+        this.router.navigate(['']);
+        // köszönjük!
+        // contact adatok kirítése
+      }).catch(() => {
+        // próbálja később
+      });
+    }
 
-}
+
+  }/*submit*/
+
+    isValidName(): boolean {
+      const nameRegexp = /[A-Z][a-zA-Z][^#&<>\"~;$^%{}?]{1,20}$/;
+      return this.contact.name !== '' && nameRegexp.test(this.contact.name);
+    }
+
+}/*class*/
