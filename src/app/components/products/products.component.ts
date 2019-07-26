dab346c9f2e986ae1c107b46c6ea4690cdbcf930
kerpeter1 <kerpeter1@gmail.com>
2019-07-26 10:05:53 +0200

    create page-button component

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -3,20 +3,35 @@ import { WineCard } from 'src/app/interfaces/wine';
 import { ActivatedRoute } from '@angular/router';
 import { ProductHttpService } from 'src/app/services/product-http.service';
 import { FilterSettings, Category } from 'src/app/interfaces/filter-settings';
+import { PageButton } from 'src/app/interfaces/page-button';
 
+
+const pageButtonsCount:number=5;
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
+
 export class ProductsComponent implements OnInit {
 
   wineCards: WineCard[];
   filterSettings: FilterSettings;
+  pageButtons: PageButton[];
   currentPage: number;
 
   constructor(private productHttpService: ProductHttpService, private route: ActivatedRoute) {
+    this.pageButtons=[];
+    for (let i = 0; i < pageButtonsCount; i++) {
+        this.pageButtons[i]={
+          nr:i+1,
+          active:false
+        }
+    }
+    this.pageButtons[3].active=true;
+    console.log("test1");
+    console.log(this.pageButtons);
     this.wineCards = [];
     this.filterSettings = {};
   }
@@ -36,10 +51,8 @@ export class ProductsComponent implements OnInit {
 
   refresh(filterSettings?: FilterSettings, pageNumber?: number) {
     this.filterSettings = filterSettings;
-    console.log(filterSettings);
     this.productHttpService.getWines(this.cleanedFilter(this.filterSettings)).then(wineCardResults => {
       this.wineCards = wineCardResults.wines;
-      console.log(this.wineCards);
 
       if (!pageNumber && this.wineCards.length > 0) {
         this.currentPage = 1;
