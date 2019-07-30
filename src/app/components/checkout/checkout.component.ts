5322690cfd3ed726cf9e15d46eb27a9a5062cab5
Judit Szekeres <szekeresjuci1@gmail.com>
2019-07-30 11:30:01 +0200

    Countries dropdown populated; 'billing address same as shipping' checkbox works

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,48 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import { Address } from '../../interfaces/address';
-import { User } from "src/app/interfaces/user";
+import { UserShipping } from "src/app/interfaces/user-shipping";
+import { UserHttpService } from "src/app/services/user-http.service";
+import { Country } from "src/app/interfaces/country";
 
 @Component({
-  selector: 'app-checkout',
-  templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+    selector: "app-checkout",
+    templateUrl: "./checkout.component.html",
+    styleUrls: ["./checkout.component.css"]
 })
 export class CheckoutComponent implements OnInit {
 
-  shippingAddress: Address;
-  billingAddress: Address;
-  checkAddress: boolean;
+    countries: Country[];
+    checkboxState: boolean;
+    userShipping: UserShipping;
 
-  
-
-  constructor(  ) {
-    this.shippingAddress = {
-      firstname: '',
-      lastname: '',
-      street: '',
-      city: '',
-      country: ''
+    constructor( private checkoutRequest: UserHttpService ) {
+        this.userShipping = {
+            shippingAddress: {
+                firstName: "",
+                lastName: "",
+                street: "",
+                city: "",
+                id: -1
+            },
+            billingAddress: {
+                firstName: "",
+                lastName: "",
+                street: "",
+                city: "",
+                id: -1
+            }
+        };
+        this.countries = [];
+        this.checkboxState = false;
     }
 
-    this.billingAddress = {
-      firstname: '',
-      lastname: '',
-      street: '',
-      city: '',
-      country: ''
-    };
-    console.log("eddig jó");
-  }
-
-  ngOnInit() {
-
-  }
-
-  setBilling() {
-    if (this.checkAddress) {
-      
+    ngOnInit() {
+        this.checkoutRequest.getCountriesList().then(c => {
+            this.countries = c;
+        });
     }
 
-  }
-
+    billingSameAsShipping() {
+        if (this.checkboxState == false) {
+            this.userShipping.billingAddress = this.userShipping.shippingAddress;
+        } else {
+            this.userShipping.shippingAddress = {
+                firstName: "",
+                lastName: "",
+                street: "",
+                city: "",
+                id: -1
+            };
+        }
+    }
 }
