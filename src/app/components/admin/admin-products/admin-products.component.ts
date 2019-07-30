b7244e25b7861eb56d0d1d70d50920c4ebec95a1
kerpeter1 <kerpeter1@gmail.com>
2019-07-30 14:08:59 +0200

    overwrite admin-products component (copied from products component)

--- src/app/components/admin/admin-products/admin-products.component.ts
+++ src/app/components/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FilterSettings } from 'src/app/interfaces/filter-settings';
+import { EmptyFilterSettingsService } from 'src/app/services/empty-filter-settings.service';
+import { ProductHttpService } from 'src/app/services/product-http.service';
+import { WineCard } from 'src/app/interfaces/wine';
 
 @Component({
   selector: 'admin-products',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProductsComponent implements OnInit {
 
-  constructor() { }
+  pageCount: number;
 
-  ngOnInit() {
+  wineCards: WineCard[];
+  filterSettings: FilterSettings;
+  currentPage: number;
+  firstPageInBlock: number;
+
+
+  constructor(private productHttpService: ProductHttpService, private emptyFilterSettingsService: EmptyFilterSettingsService) {
+      this.wineCards = [];
+      this.filterSettings = {};
+      this.firstPageInBlock=1;
+  }
+
+  ngOnInit() { }
+
+  //Refresh whole list, set current page to 1th page => for filter
+  refreshWholeList(filterSettings?: FilterSettings) {
+      this.firstPageInBlock=1;
+      filterSettings.offset=undefined;
+      this.refresh(filterSettings);
+  }
+
+  //Refresh only a certain page => for pagination
+  refreshPage(pageNumber: number) {
+      if (!this.filterSettings) {
+          this.filterSettings = this.emptyFilterSettingsService.emptyObject();
+      }
+      this.filterSettings.offset = pageNumber;
+      this.refresh(this.filterSettings);
+  }
+
+  refresh(filterSettings?: FilterSettings) {
+      this.filterSettings = filterSettings;
+      let p = this.productHttpService.getWines(this.cleanedFilter(this.filterSettings));
+      p.then(wineCardResults => {
+          this.wineCards = wineCardResults.wines;
+          this.pageCount = wineCardResults.numberOfPage;
+      });
+  };
+
+  cleanedFilter(filterSettings?: FilterSettings) {
+      if (filterSettings) {
+          for (let key of Object.keys(filterSettings)) {
+              if (!filterSettings[key]) {
+                  filterSettings[key] = undefined;
+              }
+          }
+      }
+      return filterSettings;
   }
 
 }
