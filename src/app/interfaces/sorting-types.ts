eda3669e38ac79ba706e6d9f5eb36f185e6486de
kerpeter1 <kerpeter1@gmail.com>
2019-07-31 10:11:35 +0200

    create sort-type enum

---
+++ src/app/interfaces/sorting-types.ts
@@ -0,0 +1,10 @@
+export enum SortingType{
+  nameA='name ↑',
+  nameD='name ↓',
+  priceA='price ↑',
+  priceD='price ↓',
+  yearA='year ↑',
+  yearD='year ↓',
+  ratingA='rating ↑',
+  ratingD='rating ↓',
+}
