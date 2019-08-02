e7416cf91f889adb2cb5079bec36516c30918ce0
Bence Cserfalvi <bence.cserfalvi@gmail.com>
2019-08-02 11:22:40 +0200

    Modify products

--- src/app/components/admin/admin-products/admin-modify-product/admin-modify-product.component.ts
+++ src/app/components/admin/admin-products/admin-modify-product/admin-modify-product.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { WineDetails } from 'src/app/interfaces/wine-details';
 import { WineDetailsHttpService } from 'src/app/services/wine-details-http.service';
 import { ActivatedRoute } from '@angular/router';
+import { WineByAdmin } from 'src/app/interfaces/admin-wine';
+import { WineCard } from 'src/app/interfaces/wine';
 
 @Component({
     selector: 'admin-modify-product',
@@ -11,18 +12,42 @@ import { ActivatedRoute } from '@angular/router';
 export class AdminModifyProductComponent implements OnInit {
 
     @Input()
-    wineDetails: WineDetails;
-
+    wineByAdmin: WineByAdmin;
     wineId: number;
+    wineCard: WineCard;
+
+    wineImage : string;
 
     constructor(private route: ActivatedRoute, private wineDetailsHttpService: WineDetailsHttpService) {
         this.wineId = + this.route.snapshot.paramMap.get("id");
+    //    this.wineDetails.img = "";
+        this.wineByAdmin = {
+            name: '',
+            volume: null,
+            stock: null,
+            price: null,
+            salePrice: 0,
+            wineTypeId: null,
+            wineryId: null,
+            alcoholRating: null,
+            year: null,
+        }
+        this.wineImage = "";
     }
 
     ngOnInit() {
         this.wineDetailsHttpService.getWine(this.wineId).then(wineDetails => {
             console.log(wineDetails);
-            this.wineDetails = wineDetails;
+            this.wineByAdmin.name = wineDetails.name;
+            this.wineByAdmin.stock = wineDetails.stock;
+            this.wineByAdmin.volume = wineDetails.volume;
+            this.wineByAdmin.price = wineDetails.price;
+            this.wineByAdmin.salePrice = wineDetails.salePrice;
+            this.wineByAdmin.wineTypeId = wineDetails.wineType.id;
+            this.wineByAdmin.wineryId = wineDetails.winery.id;
+            this.wineByAdmin.alcoholRating = wineDetails.alcoholRating;
+            this.wineByAdmin.year = wineDetails.year;
+            this.wineImage= wineDetails.img;
         })
     }
 
